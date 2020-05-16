1dc02048295845f3a68e44aa784148522e6deb34
Terence Kam <t.kam@f5.com>
2020-05-16 15:19:17 +0900

    comment out more lines

--- contrib/terraform/openstack/modules/compute/main.tf
+++ contrib/terraform/openstack/modules/compute/main.tf
@@ -195,9 +195,9 @@ resource "openstack_compute_instance_v2" "k8s_master" {
     use_access_ip    = "${var.use_access_ip}"
   }
 
-  provisioner "local-exec" {
-    command = "sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, var.k8s_master_fips), 0)}/ > group_vars/no-floating.yml"
-  }
+  // provisioner "local-exec" {
+  //   command = "sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, var.k8s_master_fips), 0)}/ > group_vars/no-floating.yml"
+  // }
 }
 
 resource "openstack_compute_instance_v2" "k8s_master_no_etcd" {
@@ -243,9 +243,9 @@ resource "openstack_compute_instance_v2" "k8s_master_no_etcd" {
     use_access_ip    = "${var.use_access_ip}"
   }
 
-  provisioner "local-exec" {
-    command = "sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, var.k8s_master_fips), 0)}/ > group_vars/no-floating.yml"
-  }
+  // provisioner "local-exec" {
+  //   command = "sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, var.k8s_master_fips), 0)}/ > group_vars/no-floating.yml"
+  // }
 }
 
 resource "openstack_compute_instance_v2" "etcd" {
@@ -417,9 +417,9 @@ resource "openstack_compute_instance_v2" "k8s_node" {
     use_access_ip    = "${var.use_access_ip}"
   }
 
-  provisioner "local-exec" {
-    command = "sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, var.k8s_node_fips), 0)}/ > group_vars/no-floating.yml"
-  }
+  // provisioner "local-exec" {
+  //   command = "sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, var.k8s_node_fips), 0)}/ > group_vars/no-floating.yml"
+  // }
 }
 
 resource "openstack_compute_instance_v2" "k8s_node_no_floating_ip" {
@@ -507,58 +507,58 @@ resource "openstack_compute_instance_v2" "k8s_nodes" {
     use_access_ip    = "${var.use_access_ip}"
   }
 
-  provisioner "local-exec" {
-    command = "%{if each.value.floating_ip}sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, [for key, value in var.k8s_nodes_fips : value.address]), 0)}/ > group_vars/no-floating.yml%{else}true%{endif}"
-  }
+  // provisioner "local-exec" {
+  //   command = "%{if each.value.floating_ip}sed s/USER/${var.ssh_user}/ ../../contrib/terraform/openstack/ansible_bastion_template.txt | sed s/BASTION_ADDRESS/${element(concat(var.bastion_fips, [for key, value in var.k8s_nodes_fips : value.address]), 0)}/ > group_vars/no-floating.yml%{else}true%{endif}"
+  // }
 }
 
-resource "openstack_compute_instance_v2" "glusterfs_node_no_floating_ip" {
-  name              = "${var.cluster_name}-gfs-node-nf-${count.index + 1}"
-  count             = "${var.number_of_gfs_nodes_no_floating_ip}"
-  availability_zone = "${element(var.az_list, count.index)}"
-  image_name        = "${var.image_gfs}"
-  flavor_id         = "${var.flavor_gfs_node}"
-  key_pair          = "${openstack_compute_keypair_v2.k8s.name}"
+// resource "openstack_compute_instance_v2" "glusterfs_node_no_floating_ip" {
+//   name              = "${var.cluster_name}-gfs-node-nf-${count.index + 1}"
+//   count             = "${var.number_of_gfs_nodes_no_floating_ip}"
+//   availability_zone = "${element(var.az_list, count.index)}"
+//   image_name        = "${var.image_gfs}"
+//   flavor_id         = "${var.flavor_gfs_node}"
+//   key_pair          = "${openstack_compute_keypair_v2.k8s.name}"
 
-  dynamic "block_device" {
-    for_each = var.gfs_root_volume_size_in_gb > 0 ? [var.image] : []
-    content {
-      uuid                  = "${data.openstack_images_image_v2.vm_image.id}"
-      source_type           = "image"
-      volume_size           = "${var.gfs_root_volume_size_in_gb}"
-      boot_index            = 0
-      destination_type      = "volume"
-      delete_on_termination = true
-    }
-  }
+//   dynamic "block_device" {
+//     for_each = var.gfs_root_volume_size_in_gb > 0 ? [var.image] : []
+//     content {
+//       uuid                  = "${data.openstack_images_image_v2.vm_image.id}"
+//       source_type           = "image"
+//       volume_size           = "${var.gfs_root_volume_size_in_gb}"
+//       boot_index            = 0
+//       destination_type      = "volume"
+//       delete_on_termination = true
+//     }
+//   }
 
-  network {
-    name = "${var.network_name}"
-  }
+//   network {
+//     name = "${var.network_name}"
+//   }
 
-  security_groups = ["${openstack_networking_secgroup_v2.k8s.name}"]
+//   security_groups = ["${openstack_networking_secgroup_v2.k8s.name}"]
 
-  dynamic "scheduler_hints" {
-    for_each = var.use_server_groups ? [openstack_compute_servergroup_v2.k8s_node[0]] : []
-    content {
-      group = "${openstack_compute_servergroup_v2.k8s_node[0].id}"
-    }
-  }
+//   dynamic "scheduler_hints" {
+//     for_each = var.use_server_groups ? [openstack_compute_servergroup_v2.k8s_node[0]] : []
+//     content {
+//       group = "${openstack_compute_servergroup_v2.k8s_node[0].id}"
+//     }
+//   }
 
-  metadata = {
-    ssh_user         = "${var.ssh_user_gfs}"
-    kubespray_groups = "gfs-cluster,network-storage,no-floating"
-    depends_on       = "${var.network_id}"
-    use_access_ip    = "${var.use_access_ip}"
-  }
-}
+//   metadata = {
+//     ssh_user         = "${var.ssh_user_gfs}"
+//     kubespray_groups = "gfs-cluster,network-storage,no-floating"
+//     depends_on       = "${var.network_id}"
+//     use_access_ip    = "${var.use_access_ip}"
+//   }
+// }
 
-resource "openstack_compute_floatingip_associate_v2" "bastion" {
-  count                 = "${var.number_of_bastions}"
-  floating_ip           = "${var.bastion_fips[count.index]}"
-  instance_id           = "${element(openstack_compute_instance_v2.bastion.*.id, count.index)}"
-  wait_until_associated = "${var.wait_for_floatingip}"
-}
+// resource "openstack_compute_floatingip_associate_v2" "bastion" {
+//   count                 = "${var.number_of_bastions}"
+//   floating_ip           = "${var.bastion_fips[count.index]}"
+//   instance_id           = "${element(openstack_compute_instance_v2.bastion.*.id, count.index)}"
+//   wait_until_associated = "${var.wait_for_floatingip}"
+// }
 
 
 resource "openstack_compute_floatingip_associate_v2" "k8s_master" {
@@ -588,15 +588,15 @@ resource "openstack_compute_floatingip_associate_v2" "k8s_nodes" {
   wait_until_associated = "${var.wait_for_floatingip}"
 }
 
-resource "openstack_blockstorage_volume_v2" "glusterfs_volume" {
-  name        = "${var.cluster_name}-glusterfs_volume-${count.index + 1}"
-  count       = "${var.gfs_root_volume_size_in_gb == 0 ? var.number_of_gfs_nodes_no_floating_ip : 0}"
-  description = "Non-ephemeral volume for GlusterFS"
-  size        = "${var.gfs_volume_size_in_gb}"
-}
+// resource "openstack_blockstorage_volume_v2" "glusterfs_volume" {
+//   name        = "${var.cluster_name}-glusterfs_volume-${count.index + 1}"
+//   count       = "${var.gfs_root_volume_size_in_gb == 0 ? var.number_of_gfs_nodes_no_floating_ip : 0}"
+//   description = "Non-ephemeral volume for GlusterFS"
+//   size        = "${var.gfs_volume_size_in_gb}"
+// }
 
-resource "openstack_compute_volume_attach_v2" "glusterfs_volume" {
-  count       = "${var.gfs_root_volume_size_in_gb == 0 ? var.number_of_gfs_nodes_no_floating_ip : 0}"
-  instance_id = "${element(openstack_compute_instance_v2.glusterfs_node_no_floating_ip.*.id, count.index)}"
-  volume_id   = "${element(openstack_blockstorage_volume_v2.glusterfs_volume.*.id, count.index)}"
-}
+// resource "openstack_compute_volume_attach_v2" "glusterfs_volume" {
+//   count       = "${var.gfs_root_volume_size_in_gb == 0 ? var.number_of_gfs_nodes_no_floating_ip : 0}"
+//   instance_id = "${element(openstack_compute_instance_v2.glusterfs_node_no_floating_ip.*.id, count.index)}"
+//   volume_id   = "${element(openstack_blockstorage_volume_v2.glusterfs_volume.*.id, count.index)}"
+// }
